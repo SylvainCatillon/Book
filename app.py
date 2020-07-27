167dd5eb2183f0698234ef8bd56eba9017206114
SylvainCatillon <sylvain.catillon@gmail.com>
2020-07-27 16:51:46 +0200

    scrollIntoView requirements.txt

--- app.py
+++ app.py
@@ -9,25 +9,29 @@ def index():
         "title": "PlanniTeam",
         "subtitle": "Application Django - Projet libre",
         "tag": "planniteam",
-        "img": url_for('static', filename='img/PlanniTeam.jpg')
+        "btn_img": url_for('static', filename='img/PlanniTeam.jpg'),
+        "content_img": url_for('static', filename='img/planniteam.jpg')
         },
         {
         "title": "Pur Beurre",
         "subtitle": "Application Django - Projet suivant un cahier des charges",
         "tag": "purbeurre",
-        "img": url_for('static', filename='img/purbeurre.jpg')
+        "btn_img": url_for('static', filename='img/purbeurre.jpg'),
+        "content_img": url_for('static', filename='img/charte.jpg')
         },
         {
         "title": "GrandPy Bot",
         "subtitle": "Application Flask - Projet avec API",
         "tag": "grandpy",
-        "img": url_for('static', filename='img/grandpy.jpg')
+        "btn_img": url_for('static', filename='img/grandpy.jpg'),
+        "content_img": url_for('static', filename='img/slide_grandpy.jpg')
         },
         {
         "title": "OCPizza",
         "subtitle": "Spécifications fonctionelles et techniques",
         "tag": "ocpizza",
-        "img": url_for('static', filename='img/ocpizza.jpg')
+        "btn_img": url_for('static', filename='img/ocpizza.jpg'),
+        "content_img": url_for('static', filename='img/ocpizza.jpg')
         }
     ]
     return render_template('index.html', projects=projects)
